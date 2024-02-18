67337d495adf7509860c60aaedb12a701486ba42
n mnn <153140996+pretzer25@users.noreply.github.com>
2024-02-18 17:44:46 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,54 +1,21 @@
-// Configuring Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyDk273lPBWt6ejpTT5wjsR0NpHrH3JdmH0",
-    authDomain: "contact-form-7ad80.firebaseapp.com",
-    databaseURL: "https://contact-form-7ad80-default-rtdb.firebaseio.com",
-    projectId: "contact-form-7ad80",
-    storageBucket: "contact-form-7ad80.appspot.com",
-    messagingSenderId: "122230002002",
-    appId: "1:122230002002:web:8500a9ee74483e3480ea8b"
-};
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAQYdEIrsksIn6Rye1TZ9ZoyrLHU6jfHg4",
+    authDomain: "max-learn-b0415.firebaseapp.com",
+    projectId: "max-learn-b0415",
+    storageBucket: "max-learn-b0415.appspot.com",
+    messagingSenderId: "530326840871",
+    appId: "1:530326840871:web:7c2f6df90be90127409002",
+    measurementId: "G-RS36TEXZFH"
+  };
 
-// Referencing Database
-let contactFormDB = firebase.database().ref('contactForm');
-
-document.getElementById('contactForm').addEventListener('submit', submitForm);
-
-// Submit the form
-function submitForm(e) {
-    e.preventDefault();
-
-    let name = getElementVal('name');
-    let email = getElementVal('email');
-    let message = getElementVal('message');
-
-    saveMessages(name, email, message);
-
-    // Enable Alert
-    document.getElementById('alert').style.display = 'block';
-
-    // Remove Alert
-    setTimeout(() => {
-        document.getElementById('alert').style.display = 'none';
-    }, 3000);
-
-    // Reset the form
-    document.getElementById('contactForm').reset();
-}
-
-// Saving the messages
-const saveMessages = (name, email, message) => {
-    let newContactForm = contactFormDB.push();
-    newContactForm.set({
-        name: name,
-        email: email,
-        message: message
-    })
-};
-
-const getElementVal = (id) => {
-    return document.getElementById(id).value;
-};
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
